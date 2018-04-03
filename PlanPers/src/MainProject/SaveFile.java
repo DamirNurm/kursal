daaf69ba3f232737ddfd46eb19f2954530da3668
Morfg <Morfg@Aspire-V3-772>
2018-04-03 18:08:06 +0500

    Изменено диалоговое окно, информирующее о сохранении. Изменён способ сохранения информации.

--- PlanPers/src/MainProject/SaveFile.java
+++ PlanPers/src/MainProject/SaveFile.java
@@ -3,26 +3,26 @@ package MainProject;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
+import javax.swing.JOptionPane;
+
 public class SaveFile {
 	public static void SaveInFile() {
 		try {
 			PrintWriter writer = new PrintWriter("Results.txt");
-			writer.println("=======================");
-			writer.println("TOTAL AREA: " + place.totalarea);
-			writer.println("FREE AREA: " + place.freearea);
-			writer.println("Amount of objects: " + AmountObj.amount);
+			writer.println("======================="
+					+"\nTOTAL AREA: " + place.totalarea + 
+					"\nFREE AREA: " + place.freearea + 
+					"\nAmount of objects: " + AmountObj.amount);
 			
 			for (int i=1;i<=AmountObj.amount;i++) {
-			writer.println("=======================");
-			writer.println("Number Object: " + i);
-			writer.println("Width: " + MainProject.Objects.Obj[i].width);
-			System.out.println(Objects.Obj[i].width);
-			
-			writer.println("Length: " + Objects.Obj[i].length);
-			writer.println("Occupies area: " + Objects.Obj[i].area);
+			writer.println(
+					"=======================\nNumber Object:" + i + 
+					"\nWidth: " + MainProject.Objects.Obj[i].width + 
+					"\nLength: " + Objects.Obj[i].length + 
+					"\nOccupies area: " + Objects.Obj[i].area);
 			}
 			writer.close();
-	    	 MainProject.SuccessSave.main(null);
+	    	JOptionPane.showMessageDialog(null, "Сохранение выполнено успешно");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
