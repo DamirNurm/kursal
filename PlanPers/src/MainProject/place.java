8f223f5727d978a2dd5d6cb21089893bdf6c5a69
damir <damir@Aspire-V3-772>
2018-04-04 20:30:50 +0500

    Добавлены комментарии

--- PlanPers/src/MainProject/place.java
+++ PlanPers/src/MainProject/place.java
@@ -1,25 +1,79 @@
+/**
+* Устанавливаем принадлежность класса к пакету
+ */
 package MainProject;
-
+/**
+* Подключаем библиотеку для работы с диалоговыми окнами
+ */
 import javax.swing.JOptionPane;
 
-public class place extends Objects{
+/**
+ * Объявляем класс с модификатором public
+ */
+public class place {
+	/**
+	 * Объявляем переменные
+	 * totalarea - вся площадь комнаты
+	 * freearea - незанятая площадь в комнате
+	 */
 	public static float totalarea = 0;
 	public static float freearea = 0;
-
+	/**
+	 * Метод AreaCounter
+	 */
 	public static void AreaCounter() {
+		/**
+		 * Выполняем отслеживание блока кода, где может произойти ошибка, при помощи
+		 * исключения try
+		 */
 		try {
+			/**
+			 * Присваиваем атрибутам класса значения
+			 */
 			MainProject.place.totalarea = Float.parseFloat(MenuPlanirov.textField.getText());
 			MainProject.place.freearea = Float.parseFloat(MenuPlanirov.textField.getText());
+			/**
+			 * Высчитываем значение незанятой площади в комнате
+			 */
 			for (int i = 1; i <= AmountObj.amount; i++) {
 				MainProject.place.freearea -= Objects.Obj[i].area;
 			}
+			/**
+			 * Если незанятой площади становится меньше 0, то
+			 * присваиваем значение 0.
+			 */
 			if(freearea<0)
 				freearea=0;
+			/**
+			 * Записываем результат расчёта незанятого пространства в
+			 * форму планировщика.
+			 */
 			MenuPlanirov.textField_3.setText(String.valueOf(MainProject.place.freearea));
-	}catch (Exception ep) {JOptionPane.showMessageDialog(null, "Ошибка во время расчётов! \nЗначение площади комнаты заполнено неверно!");}
+			
 	}
+		/**
+		 * Исключение catch
+		 */
+		catch (Exception ep) {
+			/**
+			 * Показываем диалоговое окна с информацией об ошибке
+			 */
+		JOptionPane.showMessageDialog(null, "Ошибка во время расчётов! \nЗначение площади комнаты заполнено неверно!");
+		}
+	}
+	/**
+	 * Метод SetTotalArea
+	 */
 	public static void SetTotalArea() {
+		/**
+		 * Проверяем необходимость заполнения поля 
+		 * в форме "Планировщик"
+		 */
 		if (totalarea!=0.0)
+			/**
+			 * Заполняем поле textField в форме "Планировщик"
+			 * значением всей площади комнаты.
+			 */
 		MainProject.MenuPlanirov.textField.setText(String.valueOf(Math.round(MainProject.place.totalarea)));
 	}
 	
