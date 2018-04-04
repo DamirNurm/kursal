79702534bab9bc90fc6047cf49f8951071b3f107
damir <damir@Aspire-V3-772>
2018-04-04 19:55:23 +0500

    Добавлены комментарии

--- PlanPers/src/MainProject/DigitFilter.java
+++ PlanPers/src/MainProject/DigitFilter.java
@@ -1,18 +1,59 @@
+/**
+* Устанавливаем принадлежность класса к пакету
+ */
 package MainProject;
 
-
+/**
+ * Подключаем библиотеку для работы с графическим интерфейсом
+ */
 import javax.swing.JTextField;
+
+/**
+ * Подключаем библиотеку для работы с набором уникальных атрибутов
+ */
 import javax.swing.text.AttributeSet;
+/**
+ * Подключаем библиотеку для исключения попытки
+ * сослаться на расположение, которое не существует.
+ */
 import javax.swing.text.BadLocationException;
+/**
+ * Подключаем библиотеку для работы с простым документом
+ */
 import javax.swing.text.PlainDocument;
 
+/**
+ * Объявляем класс с модификатором public
+ */
 public class DigitFilter {
+	
+	/**
+	 * Метод TextFilter, который принимает на вход
+	 * два параметра TextField и length(длина строки).
+	 */
 	public static void TextFilter(JTextField TextField, int length) {
+		/**
+		 * Вставляем фильтр вводимых символов
+		 */
 		TextField.setDocument(new PlainDocument() {
 			String chars = "0123456789.";
+			/**
+			 * Операции занесения текста в документ методом insertString().
+			 * Для защиты метода от исключений используем список
+			 * исключений(BadLocationException).
+			 */
 			public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+				/**
+				 * Проверка отсутствие пустой строки
+				 */
 				if (chars.indexOf(str) != -1) {
+					/**
+					 * Проверка длины введённых символов
+					 */
 					if (getLength() < length) {
+						/**
+						 * Ввод символов
+						 */
 						super.insertString(offs, str, a);
 					}
 				}
